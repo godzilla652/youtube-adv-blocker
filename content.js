cd3b607c60e083af059c7aaa23db6b3974e7c119
godzilla652 <godzillagoeshard@gmail.com>
2018-10-10 19:08:29 +0300

    refresh hosts && refresh advs

--- content.js
+++ content.js
@@ -1,35 +1,48 @@
+function eraseElement(queryElement){
+	let temp = document.querySelector(queryElement)
+	if(temp){
+			temp.parentNode.removeChild(temp)
+	}
+}
+
 setInterval(check, 2000)
 function check(){
-	let temp = null
 
-	temp = document.querySelector("div[id*='goog'")
-	if(temp){
-		temp.parentNode.removeChild(temp)
-		temp = null
-	}
+	eraseElement("div[id*='goog'")
+	eraseElement(".ad-container.ad-container-single-media-element-annotations.ad-overlay")
+	eraseElement("div[class='video-annotations']")
+	eraseElement("div[class='annotation annotation-type-custom iv-promo']")
+	eraseElement("div[class='annotation annotation-type-custom iv-branding']")
+	eraseElement("div[class='ytp-chrome-top ytp-share-button-visible']")
 
-	temp = document.querySelector(".ad-container.ad-container-single-media-element-annotations.ad-overlay")
-	if(temp){
-		temp.parentNode.removeChild(temp)
-		temp = null
-	}
-
-	temp = document.querySelector("div[class='video-annotations']")
-	if(temp){
-		temp.parentNode.removeChild(temp)
-		temp = null
-	}
-
-	temp = document.querySelector("div[class='annotation annotation-type-custom iv-promo']")
-	if(temp){
-		temp.parentNode.removeChild(temp)
-		temp = null
-	}
-
-	temp = document.querySelector("div[class='annotation annotation-type-custom iv-branding']")
-	if(temp){
-		temp.parentNode.removeChild(temp)
-		temp = null
-	}
+	// temp = document.querySelector("div[id*='goog'")
+	// if(temp){
+	// 	temp.parentNode.removeChild(temp)
+	// 	temp = null
+	// }
+	//
+	// temp = document.querySelector(".ad-container.ad-container-single-media-element-annotations.ad-overlay")
+	// if(temp){
+	// 	temp.parentNode.removeChild(temp)
+	// 	temp = null
+	// }
+	//
+	// temp = document.querySelector("div[class='video-annotations']")
+	// if(temp){
+	// 	temp.parentNode.removeChild(temp)
+	// 	temp = null
+	// }
+	//
+	// temp = document.querySelector("div[class='annotation annotation-type-custom iv-promo']")
+	// if(temp){
+	// 	temp.parentNode.removeChild(temp)
+	// 	temp = null
+	// }
+	//
+	// temp = document.querySelector("div[class='annotation annotation-type-custom iv-branding']")
+	// if(temp){
+	// 	temp.parentNode.removeChild(temp)
+	// 	temp = null
+	// }
 
 }
