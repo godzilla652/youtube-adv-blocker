85d2b1c5ab6eb94b9bb0c0a4c2e0d6b029ac2d16
godzilla652 <godzillagoeshard@gmail.com>
2018-10-10 16:15:11 +0300

    + delete video annotations

--- content.js
+++ content.js
@@ -3,7 +3,9 @@ function check(){
 	let adv = document.querySelector("div[id*='goog'")
 	if(adv){
 		adv.parentNode.removeChild(adv)
-		let ol = document.querySelector(".ad-container.ad-container-single-media-element-annotations.ad-overlay")
-		ol.parentNode.removeChild(ol)
+		let temp = document.querySelector(".ad-container.ad-container-single-media-element-annotations.ad-overlay")
+		temp.parentNode.removeChild(temp)
+		temp = document.querySelector("div[class='video-annotations']")
+		temp.parentNode.removeChild(temp)
 	}
 }
