2734eb2aa4fa3d563f63e1dd37d16418a5866712
godzilla652 <godzillagoeshard@gmail.com>
2018-10-10 16:52:08 +0300

    left promo deleted

--- content.js
+++ content.js
@@ -1,11 +1,29 @@
 setInterval(check, 2000)
 function check(){
-	let adv = document.querySelector("div[id*='goog'")
-	if(adv){
-		adv.parentNode.removeChild(adv)
-		let temp = document.querySelector(".ad-container.ad-container-single-media-element-annotations.ad-overlay")
+	let temp = null
+
+	temp = document.querySelector("div[id*='goog'")
+	if(temp){
 		temp.parentNode.removeChild(temp)
-		temp = document.querySelector("div[class='video-annotations']")
+		temp = null
+	}
+
+	temp = document.querySelector(".ad-container.ad-container-single-media-element-annotations.ad-overlay")
+	if(temp){
 		temp.parentNode.removeChild(temp)
+		temp = null
+	}
+
+	temp = document.querySelector("div[class='video-annotations']")
+	if(temp){
+		temp.parentNode.removeChild(temp)
+		temp = null
+	}
+
+	temp = document.querySelector("div[class='annotation annotation-type-custom iv-promo']")
+	if(temp){
+		console.log(temp)
+		temp.parentNode.removeChild(temp)
+		temp = null
 	}
 }
