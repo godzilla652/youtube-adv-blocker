cac06bcecbdeca0b3b19ad3d2a62444b7bab45c1
godzilla652 <godzillagoeshard@gmail.com>
2018-10-13 16:16:35 +0300

    add skipButton

--- content.js
+++ content.js
@@ -4,13 +4,26 @@ function eraseElement(queryElement){
 			temp.parentNode.removeChild(temp)
 	}
 }
+
+// adv classes to delete DOM from player
 let advs = ["div[id*='goog'", ".ad-container.ad-container-single-media-element-annotations.ad-overlay","div[class='video-annotations']",
 "div[class='annotation annotation-type-custom iv-promo']", "div[class='annotation annotation-type-custom iv-branding']", "div[class='ytp-chrome-top ytp-share-button-visible']"]
 
+//skip button class
+let skipButtonClass = '.videoAdUiSkipButton'
 
+//  main
 setInterval(check, 2000)
+// check
 function check(){
+
+	let skipButton = document.querySelector(skipButtonClass)
+	if(skipButton){
+		skipButton.click()
+	}
+
 	for(let adv of advs){
 		eraseElement(adv)
 	}
+
 }
