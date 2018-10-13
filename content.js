daa156afdf9e702c3daf9bb98cef31b39be2e0e9
godzilla652 <godzillagoeshard@gmail.com>
2018-10-13 16:28:14 +0300

    newdawnfades

--- content.js
+++ content.js
@@ -6,8 +6,17 @@ function eraseElement(queryElement){
 }
 
 // adv classes to delete DOM from player
-let advs = ["div[id*='goog'", ".ad-container.ad-container-single-media-element-annotations.ad-overlay","div[class='video-annotations']",
-"div[class='annotation annotation-type-custom iv-promo']", "div[class='annotation annotation-type-custom iv-branding']", "div[class='ytp-chrome-top ytp-share-button-visible']"]
+let advs = [
+"div[id*='goog'",
+".ad-container.ad-container-single-media-element-annotations.ad-overlay",
+"div[class='video-annotations']",
+"div[class='annotation annotation-type-custom iv-promo']",
+"div[class='annotation annotation-type-custom iv-branding']",
+"div[class='ytp-chrome-top ytp-share-button-visible']",
+"div[class='ytp-ce-element ytp-ce-channel ytp-ce-channel-this ytp-ce-bottom-left-quad ytp-ce-size-1280 ytp-ce-element-show']",
+"div[class='ytp-ce-element ytp-ce-video ytp-ce-bottom-left-quad ytp-ce-size-1280 ytp-ce-element-show']",
+"div[class='ytp-ce-element ytp-ce-video ytp-ce-bottom-right-quad ytp-ce-size-1280 ytp-ce-element-show']"
+]
 
 //skip button class
 let skipButtonClass = '.videoAdUiSkipButton'
